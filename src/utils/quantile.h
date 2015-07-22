e89f01917e1b19e2dc183eaa1f74996fc23ce62f
tqchen <tianqi.tchen@gmail.com>
2015-07-21 21:18:55 -0700

    add fix rounding error

--- src/utils/quantile.h
+++ src/utils/quantile.h
@@ -270,6 +270,14 @@ struct WQSummary {
       } while (b != b_end);
     }
     this->size = dst - data;
+    const RType tol = 10;
+    RType err_mingap, err_maxgap, err_wgap;
+    this->FixError(&err_mingap, &err_maxgap, &err_wgap);
+    if (err_mingap > tol || err_maxgap > tol || err_wgap > tol) {
+      utils::Printf("INFO: mingap=%g, maxgap=%g, wgap=%g\n",
+                    err_mingap, err_maxgap, err_wgap);
+    }
+
     if (!this->Check("AfterCombine")) {
       utils::Printf("-----Left-----\n");
       sa.Print();
@@ -285,6 +293,34 @@ struct WQSummary {
       utils::Printf("[%lu] rmin=%g, rmax=%g, wmin=%g, v=%g\n",
                     i, data[i].rmin, data[i].rmax,
                     data[i].wmin, data[i].value);
+    }
+  }
+  // try to fix rounding error
+  // and re-establish invariance
+  inline void FixError(RType *err_mingap,
+                       RType *err_maxgap,
+                       RType *err_wgap) const {
+    *err_mingap = 0;
+    *err_maxgap = 0;
+    *err_wgap = 0;
+    RType prev_rmin = 0, prev_rmax = 0;
+    for (size_t i = 0; i < this->size; ++i) {
+      if (data[i].rmin < prev_rmin) {
+        data[i].rmin = prev_rmin;
+        *err_mingap = std::max(*err_mingap, prev_rmin - data[i].rmin);
+      } else {
+        prev_rmin = data[i].rmin;
+      }
+      if (prev_rmax > data[i].rmax) {
+        data[i].rmax = prev_rmax;
+        *err_maxgap = std::max(*err_maxgap, prev_rmax - data[i].rmax);
+      }
+      RType rmin_next = data[i].rmin_next();
+      if (rmin_next > data[i].rmax) {
+        data[i].rmax = rmin_next;
+        *err_wgap = std::max(*err_wgap, data[i].rmax - rmin_next);
+      }
+      prev_rmax = data[i].rmax;
     }
   }
   // check consistency of the summary
